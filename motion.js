00c3918ea7e28ae9db8f50b9de3da3a31b0cf249
9Kevinzhao <9kzhao@stpatrick.org>
2017-05-19 13:53:16 -0500

    Add files via upload

--- motion.js
+++ motion.js
@@ -1,21 +1,18 @@
 var exports = module.exports = {};
 
 //use this prototype to create the other functions
-exports.distance = function(v,t){
-    return v*t;
+exports.distance = function(a,v,t){
+    return v*t+(a*t^2)/2;
 };
 
-exports.distance = function(a,t){
-    return (a*t^2)/2;
-}
 exports.velocity = function(d,t){
     return d/t;
 }
 
-exports.velocity = function(a,t){
+/*exports.velocity = function(a,t){
     return(a*t)
 }
-
+*/
 exports.accelerate = function(v,t){
     return v/t;
 }
--- physics.js
+++ physics.js
@@ -13,55 +13,56 @@ function printOptions(){
     console.log("distance,velocity,acceleration,time,mass,energy");
 
 }
-printOptions()
-termianl.question("type your variables",function(strcma){
-terminal.question("What type of equation are you looking for?",function(str){
-    str.toLowerCase();
-    //you will configure your series of questions inside here.
-if(str == motion){
-    terminal.question("which variable are you solving for",function(str){
-
-            function convertToArray(strcma){
-            strcma = strcma.split(',');
-            for(let i=0;i<strcma.length;i++){
-                strcma[i] = Number(strcma[i]);
-            }
-            return strcma;
-        }
-
-        if(str==accelerate){
-            motion.accelerate(strcma[1],strcma[3])
-        }
-
-        if(str==b){}
-        if(str==t){}
-        if(str==v){}
-
-
-        /*String handeling
-          Every question receives a string as input.  Since you know that strings can be upper (65 -90) or lower (97 - 122), it helps your program if it is all in one case.
-          Use str.toLowerCase() to automatically change str to a lower case character.
-          Make this the immediate first step after each question.
-        */
-        terminal.question("My question?", function(str){
-            //If I typed in "Motion"
-            str.toLowerCase();
-            //Now str == "motion";
-        });
-    })
-}
-else if(str == energy){
-    terminal.question("which variable are you solving for?"),function(str){
-        if(str==ke){}
-        if(str==m){}
-        if(str==v){}
+function convertToArray(strcma){
+    strcma = strcma.split(',');
+    for(let i=0;i<strcma.length;i++){
+        strcma[i] = Number(strcma[i]);
     }
+    return strcma;
 }
-else{
-    alert("I'm sorry I don't know how to calculate that yet...")
-}
+
+terminal.question("What type of equation are you looking for?",function(str){
+    //you will configure your series of questions inside here.
+    if(str == "motion"){
+        terminal.question("which variable are you solving for",function(str1){
+            printOptions()
+            terminal.question("type your variables",function(strcma){
+                strcma = convertToArray(strcma);
+
+                if(str1=="a"){
+                    console.log(motion.accelerate(strcma[1],strcma[3]));
+                }
+
+                if(str1=="d"){
+                    console.log(motion.distance(strcma[2],strcma[1],strcma[3]))
+                }
+                if(str1=="t"){
+                    console.log(motion.time(strcma[0],strcma[1]))
+                }
+                if(str1=="v"){
+                    console.log(motion.velocity(strcma[0],strcma[3]))
+                }
+
+            });
+        })
+    }
+    else if(str == "energy"){
+        terminal.question("which variable are you solving for?"),function(str2){
+            if(str2=="ke"){
+                console.log(energy.kineticEnergy(strcma[4],strcmaa[1]))
+            }
+            if(str2=="m"){
+                console.log(energy.mass(strcma[5],strcma[1]))
+            }
+            if(str2=="v"){
+                console.log(energy.velocity(strcma[5],strcma[4]))
+            }
+        }
+    }
+    else{
+        console.log ("I'm sorry I don't know how to calculate that yet...")
+    }
 });
-)};
 
 
 ////////////////////////////////////////////////////////////////////////////
